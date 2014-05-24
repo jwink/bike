a160c2ee689aaee83c937017231a8706251aa690
jwink <jeff_winkler@yahoo.com>
2014-05-23 20:37:23 -0400

    for a-l

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -3,8 +3,16 @@ class BikesController < ApplicationController
   def index
     all_stations = Citibikenyc.stations["results"]
     @ferry = all_stations.select {|x| x["id"]==514}
-
-
   end
+
+  def annelaure
+    @al = []
+    all_stations = Citibikenyc.stations["results"]
+    @al << all_stations.select {|x| x["id"]==537}
+    @al << all_stations.select {|x| x["id"]==352}
+    @al << all_stations.select {|x| x["id"]==446}
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'bikes#index'
+  get '/bikes/annelaure' => 'bikes#annelaure'
 
 
 end
