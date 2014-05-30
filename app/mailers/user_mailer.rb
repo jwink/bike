10ed0bc261d68e1f51e2b7c050d87fa16cc8af0c
jwink <jeff_winkler@yahoo.com>
2014-05-30 13:29:13 -0400

    mailish

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "data@bike_data.com"
 
-  def send_it
+  def send_it()
+    #@cs = csv_string
     mail(to: "jeff_winkler@yahoo.com", subject: "Bike Test")
   end
 
